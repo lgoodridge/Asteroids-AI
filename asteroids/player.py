6063736202541c748e1139f55a48fa6389cf9fdd
Lance Goodridge <ldgoodridge95@gmail.com>
2018-05-07 11:31:16 -0400

    Fixed player ship movement engine

--- asteroids/player.py
+++ asteroids/player.py
@@ -13,14 +13,14 @@ class Player(Component):
     # Maximum speed of the player ship
     MAX_SPEED = 10
 
-    # How much boosters will increase speed per step
+    # How much boosters affect speed and angle per step
     BOOSTER_ACCELERATION = 0.1
 
     # How much speed decreases per step when not boosting
     DRAG_DECELERATION = 0.02
 
     # How many degrees (in radians) the ship rotates per step
-    ROTATE_SPEED = 0.05
+    ROTATE_SPEED = 0.08
 
     # Possible spin directions
     COUNTER_CLOCKWISE, NO_SPIN, CLOCKWISE = [-1, 0, 1]
@@ -28,6 +28,7 @@ class Player(Component):
     def __init__(self, x, y):
         super(Player, self).__init__(Player.RADIUS, x, y, 0, 0)
         self.boosting = False
+        self.rotation = 0
         self.spin = Player.NO_SPIN
 
     def move(self):
@@ -36,11 +37,19 @@ class Player(Component):
         """
         super(Player, self).move()
         if self.boosting:
-            new_speed = self.speed + Player.BOOSTER_ACCELERATION
+            new_vx = (self.speed * math.sin(self.angle)) + \
+                    (Player.BOOSTER_ACCELERATION * math.sin(self.rotation))
+            new_vy = (self.speed * math.cos(self.angle)) + \
+                    (Player.BOOSTER_ACCELERATION * math.cos(self.rotation))
+            new_speed = math.sqrt((new_vx * new_vx) + (new_vy * new_vy))
+            new_angle = math.atan2(new_vx, new_vy)
         else:
             new_speed = self.speed - Player.DRAG_DECELERATION
+            new_angle = self.angle
         self.speed = max(min(new_speed, Player.MAX_SPEED), 0)
-        self.angle = (self.angle + self.spin * Player.ROTATE_SPEED) % (2*math.pi)
+        self.angle = new_angle % (2 * math.pi)
+        self.rotation = (self.rotation + self.spin * Player.ROTATE_SPEED) \
+                % (2*math.pi)
 
     def draw(self, screen):
         """
@@ -48,8 +57,8 @@ class Player(Component):
         """
         super(Player, self).draw(screen)
         unrotated_angles = [0, (3 * math.pi / 4), (5 * math.pi / 4)]
-        vertices = [(self.x + self.radius * math.sin(p + self.angle),
-                self.y - self.radius * math.cos(p + self.angle))
+        vertices = [(self.x + self.radius * math.sin(p + self.rotation),
+                self.y - self.radius * math.cos(p + self.rotation))
                 for p in unrotated_angles]
         vertices = vertices[:2] + [(self.x, self.y)] + vertices[2:]
         pygame.draw.polygon(screen, (255, 255, 255), vertices, 1)
--- settings.py
+++ settings.py
@@ -12,3 +12,6 @@ SCREEN_EDGE_THICKNESS = 10
 # Who (or what) is playing the game
 [HUMAN, AI] = range(2)
 PLAYER_MODE = HUMAN
+
+# Whether or not to show FPS in bottom left corner
+SHOW_FPS = False
