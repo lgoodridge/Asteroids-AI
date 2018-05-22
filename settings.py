b8a29a8386f6b2923f9fa238b1791d98aedb23ab
Lance Goodridge <ldgoodridge95@gmail.com>
2018-05-21 22:30:53 -0400

    Made settings configurable via command line

--- settings.py
+++ settings.py
@@ -104,3 +104,88 @@ SHOW_FPS = True
 
 # Whether to play sound effects
 PLAY_SFX = True
+
+##################################################
+#         COMMAND LINE INTERFACE SUPPORT
+##################################################
+
+import click
+
+@click.command()
+@click.option("--run-mode", type=click.Choice(["game", "experiment"]),
+        default=None, help="Whether to play the game, or run an experiment.")
+@click.option("--player-mode", type=click.Choice(["human", "ai"]),
+        default=None, help="Who (or what) is playing the game.")
+@click.option("--game-algorithm-id", type=click.Choice(["simple"]),
+        default=None, help="Algorithm to use when AI is playing the game.")
+@click.option("--game-ai-brain", type=click.Path(exists=True),
+        default=None, help="Path to AI brain to use when playing the game.")
+@click.option("--experiment-algorithm-id", type=click.Choice(["simple"]),
+        default=None, help="Algorithm to use for the next started experiment.")
+@click.option("--experiment-directory", type=click.Path(exists=False),
+        default=None, help="Path to the directory used by the experiment.")
+@click.option("--experiment-echo-logs", type=click.Choice(["true", "false"]),
+        default=None, help="Whether to echo messages for the log to console.")
+@click.option("--max-generations", type=int,
+        default=None, help="Max # generations to run the experiment for.")
+@click.option("--progress-improvement-threshold", type=int,
+        default=None, help="Minimum fitness improvement needed for progress.")
+@click.option("--max-generations-without-progress", type=int,
+        default=None, help="Max # generations to wait without progress.")
+@click.option("--generation-population", type=int,
+        default=None, help="Number of brains in each generation.")
+@click.option("--mutation-rate", type=float,
+        default=None, help="Mutation rate when breeding generations.")
+@click.option("--fitness-score-weight", type=float,
+        default=None, help="Weight of score in the fitness function.")
+@click.option("--fitness-runtime-weight", type=float,
+        default=None, help="Weight of runtime int he fitness function.")
+
+def cli_configure_settings(run_mode, player_mode, game_algorithm_id,
+        game_ai_brain, experiment_algorithm_id, experiment_directory,
+        experiment_echo_logs, max_generations, progress_improvement_threshold,
+        max_generations_without_progress, generation_population, mutation_rate,
+        fitness_score_weight, fitness_runtime_weight):
+    """
+    Configures settings according to the command line arguments.
+
+    NOTE: To support a new setting, add it to the click options above,
+        the method arguments, the list of gloval variables, and the
+        code block in the method body.
+
+    EXTRA NOTE: Currently investigating other ways of doing this that
+        doesn't involve this massive click method...
+    """
+    global RUN_MODE, PLAYER_MODE, GAME_ALGORITHM_ID, GAME_AI_BRAIN, \
+            EXPERIMENT_ALGORITHM_ID, EXPERIMENT_DIRECTORY, EXPERIMENT_ECHO_LOGS, \
+            MAX_GENERATIONS, PROGRESS_IMPROVEMENT_THRESHOLD, \
+            MAX_GENERATIONS_WITHOUT_PROGRESS, GENERATION_POPULATION, \
+            MUTATION_RATE, FITNESS_SCORE_WEIGHT, FITNESS_RUNTIME_WEIGHT
+    if run_mode is not None:
+        RUN_MODE = {"game": GAME, "experiment": EXPERIMENT}[run_mode]
+    if player_mode is not None:
+        PLAYER_MODE = {"human": HUMAN, "ai": AI}[player_mode]
+    if game_algorithm_id is not None:
+        GAME_ALGORITHM_ID = {"simple": SIMPLE}[game_algorithm_id]
+    if game_ai_brain is not None:
+        GAME_AI_BRAIN = game_ai_brain
+    if experiment_algorithm_id is not None:
+        EXPERIMENT_ALGORITHM_ID = {"simple": SIMPLE}[experiment_algorithm_id]
+    if experiment_directory is not None:
+        EXPERIMENT_DIRECTORY = experiment_directory
+    if experiment_echo_logs is not None:
+        EXPERIMENT_ECHO_LOGS = {"true": True, "false": False}[experiment_echo_logs]
+    if max_generations is not None:
+        MAX_GENERATIONS = max_generations
+    if progress_improvement_threshold is not None:
+        PROGRESS_IMPROVEMENT_THRESHOLD = progress_improvement_threshold
+    if max_generations_without_progress is not None:
+        MAX_GENERATIONS_WITHOUT_PROGRESS = max_generations_without_progress
+    if generation_population is not None:
+        GENERATION_POPULATION = generation_population
+    if mutation_rate is not None:
+        MUTATION_RATE = mutation_rate
+    if fitness_score_weight is not None:
+        FITNESS_SCORE_WEIGHT = fitness_score_weight
+    if fitness_runtime_weight is not None:
+        FITNESS_RUNTIME_WEIGHT = fitness_runtime_weight
--- start.py
+++ start.py
@@ -8,9 +8,20 @@ from ai.ai_app import AI_App
 from ai.experiment import run_experiment
 from ai.utils import algorithm_id_to_ai_brain_class
 from asteroids.app import App
+from click.exceptions import ClickException
 import settings
+import sys
 
 if __name__ == "__main__":
+
+    # Configure settings according to command line arguments
+    try:
+        settings.cli_configure_settings(standalone_mode=False)
+    except ClickException as e:
+        e.show()
+        sys.exit(e.exit_code)
+
+    # Start the game if specified
     if settings.RUN_MODE == settings.GAME:
         if settings.PLAYER_MODE == settings.HUMAN:
             App().start_game()
@@ -19,5 +30,7 @@ if __name__ == "__main__":
                     settings.GAME_ALGORITHM_ID)
             ai_brain = ai_brain_class.load(settings.GAME_AI_BRAIN)
             AI_App().start_game(ai_brain)
+
+    # Start experiment if specified
     elif settings.RUN_MODE == settings.EXPERIMENT:
         run_experiment()
