641e6e285b3a65782c337bba5a3ca10d5b91fffc
Lance Goodridge <ldgoodridge95@gmail.com>
2018-05-27 14:42:52 -0400

    Updated .gitignore + experiment error handling
    Updated .gitignore to include the experiment folder
    Added better error handling and logging to run_experiment
    Fixed some logging bugs in experiment run_experiment

--- ai/experiment.py
+++ ai/experiment.py
@@ -14,6 +14,7 @@ import os
 import settings
 import shutil
 import sys
+import traceback
 
 def run_experiment():
     """
@@ -34,7 +35,7 @@ def run_experiment():
     # If the experiment directory doesn't exist
     # yet, create it, and start a new experiment
     if not os.path.exists(experiment_dir):
-        print("Initializing experiment directory '%s'...")
+        print("Initializing experiment directory '%s'..." % experiment_dir)
 
         # Create the experiment directory, ensuring
         # the parent directory exists beforehand
@@ -57,14 +58,18 @@ def run_experiment():
                 "Generation Index": generation_idx,
                 "Stagnation Index": stagnation_idx,
         })
-        _write_meta_file(meta_filename, meta_dict)
+        try:
+            _write_meta_file(meta_filename, meta_dict)
+        except Exception as e:
+            print("ERROR CREATING META FILE!")
+            raise e
 
         # Create the experiment log file
         log = open(log_filename, "w")
         current_time_str = datetime.now().strftime("%m/%d/%Y %H:%M")
-        print("========================================", log)
-        print("=     STARTED ON %s" % current_time_str, log)
-        print("========================================", log)
+        print("========================================", file=log)
+        print("=     STARTED ON %s" % current_time_str, file=log)
+        print("========================================", file=log)
         _write_to_log(log, "Starting experiment '%s':\n" % experiment_name, True)
 
     # If it does, resume the previous experiment
@@ -72,7 +77,11 @@ def run_experiment():
         print("Resuming experiment '%s':" % experiment_name)
 
         # Load the meta data
-        meta_dict = _load_meta_file(meta_filename)
+        try:
+            meta_dict = _load_meta_file(meta_filename)
+        except Exception as e:
+            print("ERROR LOADING META FILE!")
+            raise e
         best_fitness = meta_dict["Best Fitness"]
         best_brain_tag = meta_dict["Best Brain"]
         generation_idx = meta_dict["Generation Index"]
@@ -81,9 +90,10 @@ def run_experiment():
         # Load the log file
         log = open(log_filename, "a")
         current_time_str = datetime.now().strftime("%m/%d/%Y %H:%M")
-        print("========================================", log)
-        print("=     CONTINUED ON %s" % current_time_str, log)
-        print("========================================", log)
+        print("", file=log)
+        print("========================================", file=log)
+        print("=     CONTINUED ON %s" % current_time_str, file=log)
+        print("========================================", file=log)
 
     # If the provided location exists, but is
     # not a directory, fail and alert the user
@@ -96,9 +106,9 @@ def run_experiment():
 
     # If we are continuing an experiment, load the last completed generation
     if generation_idx > 0:
-        print("Loading generation %03d... " % generation_idx-1, end="")
+        print("Loading generation %03d... " % (generation_idx-1), end="")
         generation_dirname = os.path.join(experiment_dir,
-                "gen%03d" % generation_idx-1)
+                "gen%03d" % (generation_idx-1))
         generation = generation_class.load(generation_dirname, ai_app)
         print("Complete")
 
@@ -108,33 +118,54 @@ def run_experiment():
         generation_dirname = os.path.join(experiment_dir,
                 "gen%03d" % generation_idx)
         generation_meta_filename = os.path.join(generation_dirname,
-                Generation.META_FILENAME)
+                META_FILENAME)
 
         # End the experiment if we've reached an end condition
         if generation_idx == settings.MAX_GENERATIONS:
-            _write_to_log(log, "Max generations reached. Ending Experiment.")
+            _write_to_log(log, "Max generations reached. Ending Experiment.\n")
             break
-        if stagnation_idx >= settings.MAX_GENERATIONS_WITHOUT_PROGESS:
-            _write_to_log(log, "Progress has stagnated. Ending Experiment.")
+        if stagnation_idx >= settings.MAX_GENERATIONS_WITHOUT_PROGRESS:
+            _write_to_log(log, "Progress has stagnated. Ending Experiment.\n")
             break
 
-        # Create, evaluate, and save the generation
         if os.path.isdir(generation_dirname):
             shutil.rmtree(generation_dirname)
-        _write_to_log(log, "Generation %d: Creating " % generation_idx)
-        if generation is None:
-            generation = generation_class(generation_idx, ai_app)
-        else:
-            generation = generation.breed()
-        _write_to_log(log, "- Evaluating")
-        generation.evaluate_fitnesses()
-        best_brain_id = generation.get_best_brain_id()
-        best_brain = generation.get_brain(best_brain_id)
+
+        # Create the initial generation, or breed the next generation
+        _write_to_log(log, "Generation %d: Creating" % generation_idx)
+        try:
+            if generation is None:
+                generation = generation_class(generation_idx, ai_app)
+            else:
+                generation = generation.breed()
+        except Exception as e:
+            _write_to_log(log, "\nERROR CREATING GENERATION %d\n%s" % \
+                    (generation_idx, traceback.format_exc()), True)
+            return
+
+        # Evaluate the generation
+        _write_to_log(log, " - Evaluating")
+        try:
+            generation.evaluate_fitnesses()
+            best_brain_id = generation.get_best_brain_id()
+            best_brain = generation.get_brain(best_brain_id)
+        except Exception as e:
+            _write_to_log(log, "\nERROR EVALUATING GENERATION %d\n%s" % \
+                    (generation_idx, traceback.format_exc()), True)
+            return
         _write_to_log(log, ": Best Fitness = %.2f - Saving" % best_brain.fitness)
-        generation.save(generation_dirname)
+
+        # Save the generation
+        try:
+            generation.save(generation_dirname)
+        except Exception as e:
+            _write_to_log(log, "\nERROR SAVING GENERATION %d\n%s" % \
+                    (generation_idx, traceback.format_exc()), True)
+            return
+        _write_to_log(log, "\n")
 
         # Determine whether progress was made
-        progress_threshold = best_fitness + settings.PROGRESS_IMPROVMENT_THRESHOLD
+        progress_threshold = best_fitness + settings.PROGRESS_IMPROVEMENT_THRESHOLD
         if best_brain.fitness < progress_threshold:
             stagnation_idx += 1
 
@@ -143,23 +174,29 @@ def run_experiment():
             best_fitness = best_brain.fitness
             best_brain_tag = "Generation: %03d - ID: %03d" % \
                     (generation_idx, best_brain_id)
-            best_brain_filename = os.path.join(experiment_dir)
+            best_brain_filename = os.path.join(experiment_dir, "_best.brn")
             if os.path.exists(best_brain_filename):
                 os.remove(best_brain_filename)
-            best_brain.save(best_brain_filename)
+            try:
+                best_brain.save(best_brain_filename)
+            except Exception as e:
+                _write_to_log(log, "\nERROR SAVING BEST BRAIN OF GENERATION " + \
+                        "%d\n%s" % (generation_idx, traceback.format_exc()), True)
+                return
 
         # Update meta file
         meta_dict = OrderedDict({
                 "AI Algorithm": algorithm_name,
                 "Best Fitness": best_fitness,
                 "Best Brain": best_brain_tag,
-                "Generation Index": generation_idx,
+                "Generation Index": generation_idx+1,
                 "Stagnation Index": stagnation_idx,
         })
         _write_meta_file(meta_filename, meta_dict)
 
     # Clean up
-    close(log)
+    ai_app.cleanup_simulation()
+    log.close()
 
 def _write_to_log(log, message, force_echo=False):
     """
@@ -181,7 +218,7 @@ def _write_meta_file(filename, meta_dict):
     if os.path.exists(filename):
         os.remove(filename)
     with open(filename, "w") as meta_file:
-        json.dump(meta_file, meta_dict, indent=4)
+        json.dump(meta_dict, meta_file, indent=4)
 
 def _load_meta_file(filename):
     """
