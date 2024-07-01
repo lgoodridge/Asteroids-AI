a5e8b61d5781816d81871681d63c41be306d8d8f
Lance Goodridge <ldgoodridge95@gmail.com>
2024-06-30 20:43:43 -0400

    Updated predetermined seed parameters in examples

--- examples/simple/run.sh
+++ examples/simple/run.sh
@@ -1,1 +1,1 @@
-python start.py --run-mode=game --player-mode=ai --game-algorithm-id=simple --use-predetermined-seed=true
+python start.py --run-mode=game --player-mode=ai --game-algorithm-id=simple --use-predetermined-seeds=true
